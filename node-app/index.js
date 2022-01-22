8983e276c3a217e5f3db031b7166f75c9ddec3b8
Rhutik Giradkar <46106467+rhutikcodes@users.noreply.github.com>
2022-01-22 20:48:49 +0530

    Refactored try catch

--- node-app/index.js
+++ node-app/index.js
@@ -13,44 +13,53 @@ const zoomMeetingId = process.env.meetingId
 
 app.post('/zoomEndpoint', bodyParser.raw({ type: 'application/json' }), async (req, res) => {
     function Response(statusCode, msg) {
-        console.log(statusCode, msg);
         return res.status(statusCode).send(msg);
     }
     Response(200, 'Participant Joined')
     let eventObj;
+
     try {
         eventObj = JSON.parse(req.body);
+        if (req.headers.authorization === VERIFICATION_TOKEN) {
+
+            const meetingID = eventObj.payload.object.id;
+
+            if (eventObj.event === 'meeting.participant_joined') {
+                let participant = eventObj.payload.object.participant;
+                let participantJoinDetails = {
+                    meetingID: meetingID,
+                    name: participant.user_name,
+                    joinTime: participant.join_time,
+                    userID: participant.user_id,
+                    email: participant.email
+                };
+
+                if (meetingID === zoomMeetingId) {
+
+                    try {
+                        console.log(participantJoinDetails);
+                        const options = {
+                            method: 'POST',
+                            url: `http://localhost:5982/updateAttendance`,
+                            body: {
+                                email: participant.email,
+                            },
+                            json: true
+                        };
+
+                        await awaitRequest(options);
+                    } catch (error) {
+                        console.error(error);
+                    }
+
+                }
+            }
+        } else {
+            console.log('unauth');
+        }
     } catch (err) {
         console.log(err);
     }
-    if (req.headers.authorization === VERIFICATION_TOKEN) {
-        const meetingID = eventObj.payload.object.id;
-        
-        if (eventObj.event === 'meeting.participant_joined') {
-            let participant = eventObj.payload.object.participant;
-            let participantJoinDetails = {
-                meetingID: meetingID,
-                name: participant.user_name,
-                joinTime: participant.join_time,
-                userID: participant.user_id,
-                email: participant.email
-            };
-            if (meetingID === zoomMeetingId) {
-                const options = {
-                    method: 'POST',
-                    url: `http://localhost:5982/updateAttendance`,
-                    body: {
-                        email: participant.email,
-                    },
-                    json: true
-                };
-                await awaitRequest(options);
-                console.log(participantJoinDetails);
-            }
-           
-        }
-    }
-
 })
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
